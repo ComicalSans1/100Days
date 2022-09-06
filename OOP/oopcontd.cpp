2f2920c473a6fbd8b7cca535779764772ba803b8
Sans <hewhototallymustnotbenamed@gmail.com>
2022-09-07 00:08:22 +0530

    modified oopcontd.cpp

--- OOP/oopcontd.cpp
+++ OOP/oopcontd.cpp
@@ -2,6 +2,34 @@
 #include <string>
 using namespace std;
 
-class human() {
-    
+class Human {
+    public:
+    void lie() {
+        cout << "i am human" << endl;
+    }
+};
+
+class Idiot: public Human {
+    public:
+    void lie() {
+        cout << "kqwdfiojqwdqw" << endl;
+    }
+};
+
+class Pretentious: public Human {
+    public:
+    void lie() {
+        cout << "i have never made fun of poor people" << endl;
+    }
+};
+
+int main(void) {
+    Human featherlessBiped;
+    featherlessBiped.lie();
+
+    Idiot chicken;
+    chicken.lie();
+
+    Pretentious you;
+    you.lie();
 }
